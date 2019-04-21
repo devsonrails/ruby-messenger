ade578fadede64d5a2f3475321c369402c477d3a
Roberto Martins <roberto.martins.info@gmail.com>
2019-04-21 20:15:47 -0300

    Added action button

--- src/components/Windows/Wallpaper/Wallpaper.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Wallpaper from './Wallpaper';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Wallpaper />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
