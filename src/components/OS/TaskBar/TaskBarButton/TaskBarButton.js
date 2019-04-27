4e2cf2969551065d2c6361e0ea6f6682ca219d70
roberto.martins.info <roberto.martins.info@gmail.com>
2019-04-26 22:27:56 -0400

    Label to task action button.

--- src/components/OS/TaskBar/TaskBarButton/TaskBarButton.js
+++ src/components/OS/TaskBar/TaskBarButton/TaskBarButton.js
@@ -5,15 +5,21 @@ import image_button from './button.png';
 class TaskBarButton extends Component {
   
   state = {
-    
+    label: "MSN Messenger (BETA)",
+    icon: "",
+    window: 1234 
   }
 
   render() {
+    let {label, icon, window} = this.state
+    
     return (
       <div className="TaskBarButton" style={{
         backgroundImage: `url(${image_button})`
       }}>
-      
+        <span>
+          <i className={icon}></i>{{label}}
+        </span>
       </div>
     );
   }
